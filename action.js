20ab72cca96212a1c511545175b87731749b6741
NaySoeWinYoma <115983615+NaySoeWinYoma@users.noreply.github.com>
2023-05-09 20:58:32 +0630

    update fs

--- action.js
+++ action.js
@@ -1,8 +1,13 @@
 const core = require('@actions/core');
+const fs = require("fs")
 
 
 (() => {
     const nameToGreet = core.getInput('aws-key');
+
+    fs.readdirSync("./functions").forEach(file => {
+        console.log(file);
+    });
 
     console.log(`Hello ${nameToGreet}!`);
 })()
