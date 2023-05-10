4e4cffa5aff02cae0d45e2c24efc3123fcbbd71c
NaySoeWinYoma <115983615+NaySoeWinYoma@users.noreply.github.com>
2023-05-10 08:48:13 +0630

    update wf

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/sethvargo/go-githubactions"
 )
@@ -9,5 +11,22 @@ import (
 func main() {
 	val := githubactions.GetInput("aws-key")
 
+	if checkFolderExists("functions") {
+		fmt.Println("Ok")
+	}
+
 	fmt.Println(val)
+}
+
+func checkFolderExists(folder string) bool {
+	files, err := os.ReadDir(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name(), file.IsDir())
+	}
+
+	return true
 }
